704f64ab53a38d49d3d8a970980ec520c4f4c652
Emma-Kappen <emma.kappen@outlook.in>
2025-05-26 18:21:07 +0530

    Secrets established

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv;
+from dotenv import SECRET_KEY;
 
 load_dotenv()
 
@@ -7,5 +8,5 @@ class Config:
     # Use SQLite for testing, can be changed to MySQL later
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///leave_mgmt.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "7d105ab953fd008697d4d93b86abe182d764b565cd21c8187fbd609c74b12cf4")
+    SECRET_KEY = os.getenv("SECRET_KEY", SECRET_KEY)
 
